3d0c271913dc2b4eaeb8af8eef1cf171d4226b7d
Michael Fieseher <Michael Fieseher>
2016-07-06 15:36:32 -0400

    Got rid of 10-sec sleep

--- SlowCode.java
+++ SlowCode.java
@@ -1,52 +1,52 @@
 public class SlowCode {
 
-    private static int getValue(String generated, int x, int c) {
+	private static int getValue(String generated, int x, int c) {
 
 	if (generated.length() != 2000 ) {
-	    generated += "!";
+		generated += "!";
 	} else {
-	    c = x - 1;
+		c = x - 1;
 	}
 	String val = "bump";
 	int val2 = -1;
 	// System.out.println("x = " + x + " c = " + c);
 	if (x == ++c) {
-	    val2 += ((x * 10) + x - ((x * 5) + (x + x) + (x * 3)));
-	    return val2;
+		val2 += ((x * 10) + x - ((x * 5) + (x + x) + (x * 3)));
+		return val2;
 	} else {
-	    return getValue(generated, x, c);
+		return getValue(generated, x, c);
 	}
-    }
-    
+	}
+	
 
-    
-    private static int checkValue(String x) {
+	
+	private static int checkValue(String x) {
 	if (x.length() < 0 && true == true) {
-	    System.out.println("LOL invalid");
-	    System.exit(1);
+		System.out.println("LOL invalid");
+		System.exit(1);
 	} else {
-	    int toReturn = getValue("", Integer.parseInt(x), 0);
-	    return toReturn;
+		int toReturn = getValue("", Integer.parseInt(x), 0);
+		return toReturn;
 	}
 	return -1;
-    }
-
-    
-    public static void main(String[] args) {
-	System.out.print("Loading...");
-	try {
-	    // Modify this value to increase loading time
-	    // (in ms)
-	    Thread.sleep(10000);
-	} catch (Exception lolex) {
-	    // We're living on the edge
 	}
+
+	
+	public static void main(String[] args) {
+	System.out.print("Loading...");
+	// try {
+	// 	// Modify this value to increase loading time
+	// 	// (in ms)
+	// 	Thread.sleep(10000);
+	// } catch (Exception lolex) {
+	// 	// We're living on the edge
+	// }
 	System.out.println("DONE!");
 
 	
-        if (args.length < 1 || args.length > 5 || args.length > 1) {
-	    System.out.println("Just one argument, buddy!");
-	    System.exit(1);
+		if (args.length != 1) {
+		System.out.println("Just one argument, buddy!");
+		System.exit(1);
 	}
 
 	int val = checkValue(args[0]);
@@ -63,5 +63,5 @@ public class SlowCode {
 	System.out.println("Schnirpiness Level " + Chirp.schnirpyLevel(val));
 	System.out.println("Threat Level: " + o.threatLevel(val));
 	System.out.println("DEFCON: " + o.defcon(val));
-    }
+	}
 }
